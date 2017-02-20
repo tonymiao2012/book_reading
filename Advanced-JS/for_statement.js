81686bd5a73c449952c96ca0b0f6732b2c83dd72
tonymiao2012 <miaozixiong@gmail.com>
2017-02-20 18:52:55 +0800

    Example about for statement.

---
+++ Advanced-JS/for_statement.js
@@ -0,0 +1,13 @@
+/**
+ * Created by miaozixiong on 2017/2/20.
+ *
+ */
+for(var i = 0; i < 10; i++){
+    console.log(i);
+    log(i);
+    var p = 10;
+}
+
+function log(i){
+    console.log("i: " + i + " " + p);
+}
