4dbbf956272738984bba5ee93ab289e2c0cc3279
tonymiao2012 <miaozixiong@gmail.com>
2017-05-12 14:52:17 +0800

    Test name change

--- Advanced-JS/requireJS-test/lib/req.js
+++ Advanced-JS/requireJS-test/lib/req.js
@@ -1,7 +1,7 @@
 /**
  * Created by miaozixiong on 2017/4/1.
  */
-//Main JS module for test project. The entry of whole web page. Like main() method in C programming.
+//Ma
 
 require(['a', 'b', 'c'], function (a, b, c) {
     console.log("Main JS. Dependency requirement: a, b, c");
